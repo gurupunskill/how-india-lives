fc9698d30b2600577b04676b9e7218d05ad7fc5f
nihalh55 <nihalh55@gmail.com>
2018-10-24 15:01:48 +0530

    Selection bar added

--- public/scripts/statistics_page.js
+++ public/scripts/statistics_page.js
@@ -6,23 +6,34 @@ function S(id){
     return document.getElementById(id)
 }
 
+var closeIcon =   document.querySelectorAll('svg.close'),
+    $container = $('.container'),
+    $list = $container.find('ul'),
+    $links = $container.find('a'),
+    $text = $container.find('span');
+
+// When the '+' icon is clicked...
+$(closeIcon).on('click', function() {
+    // Add class to rotate icon to an 'x'
+    $(this).toggleClass('active');
+    // Toggle the list
+    $list.toggle();
+});
+
 $(closeIcon).hover(function() {
     $container.addClass('hover');
   }, function() {
     $container.removeClass('hover');
   });
   
-  
   $links.on('click', function() {
     $links.removeClass('active');
     
       $(this).addClass('active');
     $text.text($(this).text()).addClass('fade');
-    
     setTimeout(function(){
       $text.removeClass('fade');
     }, 800);
-    
     $list.toggle();
-    closeIcon.toggleClass('active');
+    $(closeIcon).toggleClass('active');
   });
