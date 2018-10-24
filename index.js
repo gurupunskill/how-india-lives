c355fa9cfd7b28e99a09cf8b657a7f435100303d
nihalh55 <nihalh55@gmail.com>
2018-10-25 00:54:43 +0530

    Ajax modification made

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mysql = require('mysql');
 const PORT = process.env.PORT || 5000;
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 var pool = mysql.createPool({
     host     : 'us-cdbr-iron-east-01.cleardb.net',
@@ -11,10 +12,10 @@ var pool = mysql.createPool({
     database : 'heroku_7d8b3caef3dcfea'
 });
 
-
 var app = express()
 
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -35,8 +36,11 @@ app.get('/dump', function(req,res){
     });
 });
 
-app.get('/population', function(req,res){
+app.post('/queryServer', function(req,res){
 
+    console.log(req.body.input);
+    if(req.body.input == "Total Population"){
+    
     var query = "SELECT District , TOT_P FROM pca_total LIMIT 10";
     pool.query(query,function(err,results,fields){
 
@@ -44,6 +48,7 @@ app.get('/population', function(req,res){
         res.send(results);
         console.log('Population data sent');
     });
+    }
 });
 
 
